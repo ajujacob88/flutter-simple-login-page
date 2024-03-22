01cb866355f294f6f3a975f8420208b236c089f7
ajujacob <ajujacob88@gmail.com>
2024-03-22 20:43:18 +0530

    home page - one way

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,12 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key, required this.username});
+
+  final String username;
+
+  final List<String> imagePaths = [
+    'assets/images/1.jpg',
+    'assets/images/2.jpg',
+    'assets/images/3.jpg',
+    'assets/images/4.jpg',
+    'assets/images/5.jpg',
+    'assets/images/6.jpg',
+    'assets/images/7.jpg',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Hi welcome fghhhhhhhhhh'),
+    String capitalizedUsername =
+        username.substring(0, 1).toUpperCase() + username.substring(1);
+
+    final screenHeight = MediaQuery.of(context).size.height;
+    final statusBarHeight = MediaQuery.of(context).padding.top;
+    final appBarHeight = AppBar().preferredSize.height;
+
+    const textWidgetHeight = 45.0; // Adjust this value as needed
+    final sizedBoxHeight =
+        screenHeight - statusBarHeight - appBarHeight - textWidgetHeight;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+              height: textWidgetHeight,
+              child: Center(
+                  child: Text(
+                'Hi welcome $capitalizedUsername',
+                style: const TextStyle(fontSize: 16),
+              ))),
+          SizedBox(
+            height: sizedBoxHeight,
+            child: ListView(
+              children: [
+                Image.asset(
+                  'assets/images/1.jpg',
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.fill,
+                ),
+                const SizedBox(height: 20),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image.asset(
+                    'assets/images/2.jpg',
+                    width: 200,
+                    height: 200,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Image.asset(
+                  'assets/images/3.jpg',
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.fitWidth,
+                ),
+                const SizedBox(height: 20),
+                Image.asset(
+                  'assets/images/4.jpg',
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.cover,
+                ),
+                const SizedBox(height: 20),
+                Image.asset(
+                  'assets/images/5.jpg',
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.fill,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
