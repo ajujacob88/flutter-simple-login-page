d75d87b898d540fd32eb599ddf55ec55d1905bde
ajujacob <ajujacob88@gmail.com>
2024-03-24 13:41:55 +0530

    phase 1 completed

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -76,68 +76,66 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      child: GestureDetector(
-        onTap: () {
-          // Dismiss the keyboard and unfocus the text field
-          FocusScope.of(context).requestFocus(FocusNode());
-        },
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Login Page'),
-            backgroundColor: const Color.fromARGB(255, 88, 1, 68),
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('Please use the below form to login'),
-                const SizedBox(height: 20),
-                TextField(
-                  controller: usernameController,
-                  //  onChanged: (value) => userName = value,
-                  textInputAction: TextInputAction.next,
-                  decoration: InputDecoration(
-                    labelText: 'username',
-                    border: OutlineInputBorder(
-                      gapPadding: 10,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    contentPadding: const EdgeInsets.symmetric(
-                        vertical: 15, horizontal: 20),
+    return GestureDetector(
+      onTap: () {
+        // Dismiss the keyboard and unfocus the text field
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Login Page'),
+          backgroundColor: const Color.fromARGB(255, 88, 1, 68),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Please use the below form to login'),
+              const SizedBox(height: 20),
+              TextField(
+                controller: usernameController,
+                //  onChanged: (value) => userName = value,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(
+                  labelText: 'username',
+                  border: OutlineInputBorder(
+                    gapPadding: 10,
+                    borderRadius: BorderRadius.circular(10),
                   ),
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
                 ),
-                const SizedBox(height: 15),
-                TextField(
-                  controller: passwordController,
-                  // onChanged: (value) => passWord = value,
-                  textInputAction: TextInputAction.done,
-                  onSubmitted: (_) {
-                    // Call the login function when "Done" is pressed
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                controller: passwordController,
+                // onChanged: (value) => passWord = value,
+                textInputAction: TextInputAction.done,
+                onSubmitted: (_) {
+                  // Call the login function when "Done" is pressed
+                  isValidUser(usernameController.text, passwordController.text,
+                      context);
+                },
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(
+                    gapPadding: 10,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  contentPadding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                  onPressed: () {
                     isValidUser(usernameController.text,
                         passwordController.text, context);
                   },
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(
-                      gapPadding: 10,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    contentPadding: const EdgeInsets.symmetric(
-                        vertical: 15, horizontal: 20),
-                  ),
-                  obscureText: true,
-                ),
-                const SizedBox(height: 10),
-                ElevatedButton(
-                    onPressed: () {
-                      isValidUser(usernameController.text,
-                          passwordController.text, context);
-                    },
-                    child: const Text('Login'))
-              ],
-            ),
+                  child: const Text('Login'))
+            ],
           ),
         ),
       ),
